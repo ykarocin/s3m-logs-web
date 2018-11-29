9add52e9d1ead64fe933d75ceb51d08fcac14ec8
giovanni.barros <giovanni.barros@inlocomedia.com>
2018-11-29 19:49:10 -0300

    Read case analysis from file

--- main.js
+++ main.js
@@ -1,71 +1,69 @@
 const fs = require('fs');
 const pug = require('pug');
 
+const CASES_DIR = "./cases/analyzed_conflicts/"
 const compiledFunction = pug.compileFile('template.pug');
 
-let content = "";
+function main() {
+  let content = "";
 
-fs.readdirSync("./cases").forEach((file, index) => {
-  let baseSource = fs.readFileSync("./cases/" + file + '/base')
-  let leftSource = fs.readFileSync("./cases/" + file + '/left')
-  let rightSource = fs.readFileSync("./cases/" + file + '/right')
-  let mergeMethodsSource = fs.readFileSync("./cases/" + file + '/MERGE_METHODS')
-  let keepBothMethodsSource = fs.readFileSync("./cases/" + file + '/KEEP_BOTH_METHODS')
-  let safeSource = fs.readFileSync("./cases/" + file + '/SAFE')
-  let unstructuredSource = fs.readFileSync("./cases/" + file + '/UNSTRUCTURED')
+  fs.readdirSync(CASES_DIR).forEach((file, index) => {
+    let baseSource = fs.readFileSync(CASES_DIR + file + '/base')
+    let leftSource = fs.readFileSync(CASES_DIR + file + '/left')
+    let rightSource = fs.readFileSync(CASES_DIR + file + '/right')
+    let mergeMethodsSource = fs.readFileSync(CASES_DIR + file + '/MERGE_METHODS')
+    let keepBothMethodsSource = fs.readFileSync(CASES_DIR + file + '/KEEP_BOTH_METHODS')
+    let safeSource = fs.readFileSync(CASES_DIR + file + '/SAFE')
+    let unstructuredSource = fs.readFileSync(CASES_DIR + file + '/UNSTRUCTURED')
+    let observations = fs.readFileSync(CASES_DIR + file + '/summary.html', {encoding:"UTF-8"})
 
-  let caseNumber = index + 1;
-  let title = file;
+    let caseNumber = index + 1;
+    let title = file;
 
-  // let comments = [['Construtor do Builder', ["Left alterou assinatura, corpo, e adicionou 1 overload",
-  //                 "Right alterou corpo",
-  //                 "Unstructured acusou conflito nos argumentos e no overload",
-  //                 "Safe deu match com vazio, e manteve duas outras versões do método",
-  //                 "MM e KB mantiveram três versões"]],
-  //                 ["teste", ["1", "2", 3]]
-  //             ];
-  let comments = [];
-
-  content += compiledFunction({
-    caseNumber: caseNumber,
-    title: title,
-    baseSource: baseSource,
-    leftSource: leftSource,
-    rightSource: rightSource,
-    mergeMethodsSource: mergeMethodsSource,
-    keepBothMethodsSource: keepBothMethodsSource,
-    safeSource: safeSource,
-    unstructuredSource: unstructuredSource,
-    list: comments
+    content += compiledFunction({
+      caseNumber: caseNumber,
+      title: title,
+      baseSource: baseSource,
+      leftSource: leftSource,
+      rightSource: rightSource,
+      mergeMethodsSource: mergeMethodsSource,
+      keepBothMethodsSource: keepBothMethodsSource,
+      safeSource: safeSource,
+      unstructuredSource: unstructuredSource,
+      observations: observations
+    });
   });
-});
 
-console.log(`<!doctype html>
-<html lang="en" dir="ltr">
-<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+  let htmlSource = `<!doctype html>
+    <html lang="en" dir="ltr">
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="stylesheet" type="text/css" href="dist/diff2html.min.css">
-  <link rel="stylesheet" href="dist/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="dist/diff2html.min.css">
+      <link rel="stylesheet" href="dist/bootstrap.css">
 
-  <script type="text/javascript" src="dist/diff2html.js"></script>
-  <script type="text/javascript" src="dist/jsdiff.js"></script>
+      <script type="text/javascript" src="dist/diff2html.js"></script>
+      <script type="text/javascript" src="dist/jsdiff.js"></script>
 
-  <script type="text/javascript" src="diff.js"></script>
-  
-  <title>S3M Renaming Analysis</title>
-</head>
+      <script type="text/javascript" src="diff.js"></script>
 
-<body>
-  <div class="container-fluid">
-    <h1>S3M Renaming Analysis</h1>
-    <table class="table">
-      ${content}
-    </table>
-  </div>
-</body>
-</html>
-`);
+      <title>S3M Renaming Analysis</title>
+    </head>
+
+    <body>
+      <div class="container-fluid">
+        <h1>S3M Renaming Analysis</h1>
+        <table class="table">
+          ${content}
+        </table>
+      </div>
+    </body>
+    </html>`;
+
+  console.log(htmlSource);
+}
+
+main();
